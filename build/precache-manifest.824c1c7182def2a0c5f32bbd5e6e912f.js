2d4efcc99427501f74a8ab4f1defdb4fe102e94d
Adaías Magdiel <adaiasmagdiell@gmail.com>
2019-10-21 12:57:02 -0300

    feature: criado build para rodar no heroku.

---
+++ build/precache-manifest.824c1c7182def2a0c5f32bbd5e6e912f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df3482d65f2b5dccd338bf8c3b511a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5a3e0ae781e031817a2",
+    "url": "/static/css/main.cbdfa52b.chunk.css"
+  },
+  {
+    "revision": "875bf69d41b91cb5da48",
+    "url": "/static/js/2.f4b64204.chunk.js"
+  },
+  {
+    "revision": "e5a3e0ae781e031817a2",
+    "url": "/static/js/main.87604f0e.chunk.js"
+  },
+  {
+    "revision": "8db6d69eb0b0453e86f6",
+    "url": "/static/js/runtime-main.2d9f7ca8.js"
+  },
+  {
+    "revision": "a4edba24f9a571dbb09c831694960cdf",
+    "url": "/static/media/bg.a4edba24.png"
+  },
+  {
+    "revision": "18169d195fe9b1e3d8be14d9028bd3be",
+    "url": "/static/media/natsu_kanji.18169d19.svg"
+  }
+]);
